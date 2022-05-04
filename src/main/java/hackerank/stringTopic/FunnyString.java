9d42ca4c3386dc850159b863092e311cbdb50c47
nminhthang <rlrisnguyen@gmail.com>
2022-05-04 18:42:42 +0700

    "new assignment"

---
+++ src/main/java/hackerank/stringTopic/FunnyString.java
@@ -0,0 +1,22 @@
+package hackerank.stringTopic;
+
+public class FunnyString {
+
+    public static String funnyString(String s) {
+        StringBuilder r = new StringBuilder(s);
+        r.reverse();
+        System.out.println(r);
+        for (int i = 0; i < s.length() - 1; i++) {
+            int sAscii = (int) s.charAt(i + 1) - (int) s.charAt(i);
+            int rAscii = (int) r.charAt(i + 1) - (int) r.charAt(i);
+            if (Math.abs(sAscii) != Math.abs(rAscii)) return "Not Funny";
+        }
+
+        return "Funny";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(funnyString("abc"));
+    }
+
+}
