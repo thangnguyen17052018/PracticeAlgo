c56c38603ac18b39d3aca0c0b79b237c0b612390
nminhthang <rlrisnguyen@gmail.com>
2022-05-05 10:38:45 +0700

    "new practice"

---
+++ src/main/java/hackerank/stringTopic/AlternatingCharacters.java
@@ -0,0 +1,21 @@
+package hackerank.stringTopic;
+
+public class AlternatingCharacters {
+
+    public static int alternatingCharacters(String s) {
+        int count = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i-1) == s.charAt(i)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(alternatingCharacters("AAABBB"));
+    }
+
+}
