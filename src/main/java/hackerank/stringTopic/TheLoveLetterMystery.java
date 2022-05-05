5454e4a8de2caed313558b9846ccb28a290c0fd3
nminhthang <rlrisnguyen@gmail.com>
2022-05-05 13:55:34 +0700

    "new practice"

--- src/main/java/hackerank/stringTopic/TheLoveLetterMystery.java
+++ src/main/java/hackerank/stringTopic/TheLoveLetterMystery.java
@@ -2,20 +2,23 @@ package hackerank.stringTopic;
 
 public class TheLoveLetterMystery {
 
-    public boolean checkPalindrome(String s) {
-        StringBuilder r = new StringBuilder(s);
-        r.reverse();
-        return s.equals(r);
-    }
-
     public static int theLoveLetterMystery(String s) {
+        int countReduce = 0;
 
+        for (int i = 0; i < s.length()/2; i++) {
+            countReduce += Math.abs((int) s.charAt(i) - (int) s.charAt(s.length() - i - 1));
+        }
 
-        return 1;
+        return countReduce;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(theLoveLetterMystery("abcba"));
+        System.out.println(theLoveLetterMystery("abc"));
+        System.out.println(theLoveLetterMystery("abcd"));
+        System.out.println(theLoveLetterMystery("cba"));
+        System.out.println(theLoveLetterMystery("cde"));
+        System.out.println(theLoveLetterMystery("awertr"));
     }
 
 }
