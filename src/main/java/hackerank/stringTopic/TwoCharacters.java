4a29054e4dbf3c407cc04dce922eacd3117dab82
nminhthang <rlrisnguyen@gmail.com>
2022-05-05 15:23:56 +0700

    "new practice"

--- src/main/java/hackerank/stringTopic/TwoCharacters.java
+++ src/main/java/hackerank/stringTopic/TwoCharacters.java
@@ -46,9 +46,9 @@ public class TwoCharacters {
     }
 
     public static void printMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
             }
             System.out.println();
         }
