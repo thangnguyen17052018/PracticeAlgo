ec266841353049476c83625367e38d4161cbc6dc
nminhthang <rlrisnguyen@gmail.com>
2022-05-05 11:30:38 +0700

    "new practice"

---
+++ src/main/java/hackerank/stringTopic/BeautifulBinaryString.java
@@ -0,0 +1,22 @@
+package hackerank.stringTopic;
+
+public class BeautifulBinaryString {
+
+    public static int beautifulBinaryString(String s) {
+        int count = 0;
+
+        for (int i = 1; i < s.length() -1; i++) {
+            if (s.charAt(i-1) == '0' && s.charAt(i) == '1' && s.charAt(i+1) == '0') {
+                count++;
+                i+=2;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(beautifulBinaryString("01100"));
+    }
+
+}
---
+++ src/main/java/hackerank/stringTopic/TheLoveLetterMystery.java
@@ -0,0 +1,21 @@
+package hackerank.stringTopic;
+
+public class TheLoveLetterMystery {
+
+    public boolean checkPalindrome(String s) {
+        StringBuilder r = new StringBuilder(s);
+        r.reverse();
+        return s.equals(r);
+    }
+
+    public static int theLoveLetterMystery(String s) {
+
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
